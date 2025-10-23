aad8ea674a2936bc3ebf68e78cd5e355c4fa5249
hedonicadapter <sam.herman@xenit.se>
2025-10-23 09:26:39 +0200

    server side rendering fix

--- app/login/se-bankid/[instanceId]/layout.tsx
+++ app/login/se-bankid/[instanceId]/layout.tsx
@@ -1,3 +1,4 @@
+import { headers } from "next/headers";
 import { ReactNode } from "react";
 
 interface Props {
@@ -5,15 +6,24 @@ interface Props {
   children: ReactNode;
 }
 
+const getBaseUrl = async () => {
+  const headersList = await headers();
+  const host = headersList.get("x-forwarded-host") || headersList.get("host");
+  const protocol = headersList.get("x-forwarded-proto") || "http";
+  return `${protocol}://${host}`;
+};
+
 export default async function Layout({ params, children }: Props) {
   const { instanceId } = await params;
+  const baseUrl = await getBaseUrl();
+  const res = await fetch(
+    `${baseUrl}/api/styles/${instanceId}?v=${Date.now()}`,
+  );
+  const css = await res.text();
+
   return (
     <>
-      <link
-        rel="stylesheet"
-        href={`/api/styles/${instanceId}`}
-        precedence="default"
-      />
+      <style dangerouslySetInnerHTML={{ __html: css }} />
       {children}
     </>
   );
